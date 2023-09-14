f4b8aa5dfa64a5483dc67279e1a15436cc3db72d
Vladimir Alekseev <vega23071235789@gmail.com>
2023-09-14 23:58:14 +0100

    fix: fix a small error

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-let starsArr = Array.from(document.getElementsByClassName('star'));
-for(let index = 0; index < starsArr.length; index++) {
-  starsArr[index].onclick = () => {
-    for(let indexTwo = 0; indexTwo < starsArr.length; indexTwo++) {
-      if(index <= indexTwo) {
-        starsArr[indexTwo].classList.add('star_active')
+const starsArr = document.querySelectorAll('.star');
+
+for (let i = 0; i < starsArr.length; i++) {
+  starsArr[i].addEventListener('click', () => {
+    for (let j = 0; j < starsArr.length; j++) {
+      if (j >= i) {
+        starsArr[j].classList.add('star_active');
       } else {
-        starsArr[indexTwo].classList.remove('star_active')
+        starsArr[j].classList.remove('star_active');
       }
     }
-  }
+  });  
 }
